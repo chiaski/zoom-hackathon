0d0489562676019eb9c326df1a0219faea026bec
Chia <chiamisola@gmail.com>
2020-04-03 13:12:42 -0400

    setting up rooms

--- app/server.js
+++ app/server.js
@@ -3,14 +3,26 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var usernames = {};
+var rooms = {};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 
-
-
 io.on('connection', function(socket){
+    
+    socket.on('joinroom', function(room){
+        this.join(room);
+        
+        if (typeof rooms[room] === "undefined") rooms[room] = {};
+        rooms[room].count = rooms[room].count ? rooms[room].count + 1 : 1; 
+        
+        io.to(room).emit("new user", rooms[room].count);
+        console.log(rooms);
+    })
+    
   console.log('a user connected');
     
   socket.on('disconnect', function(){
@@ -20,12 +32,13 @@ io.on('connection', function(socket){
 });
 
 
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
+//
+//
+//io.on('connection', function(socket){
+//  socket.on('chat message', function(msg){
+//    console.log('message: ' + msg);
+//  });
+//});
 
 
 
